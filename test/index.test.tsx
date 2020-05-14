4ee5c03b64c17d95dc4720bab75fe0921eeb0d37
Balázs Orbán <info@balazsorban.com>
2020-05-14 23:03:09 +0200

    test: WIP add test

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,8 +1,30 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import useEventualScroll from '../src';
+import { render } from '@testing-library/react';
 
+const App = () => {
+  const [loading, setLoading] = React.useState(false);
 
-test.skip("Scrolls after delay", () => {
+  React.useEffect(() => {
+    const someAsyncOperation = async () => {
+      //...
+      setLoading(true);
+    };
+    someAsyncOperation();
+  }, []);
 
-})
+  /**
+   * By only adding this one line,
+   * the scroll to #foo will happen, even
+   * if it takes several 100 ms.
+   * (In which case navigating to this page for the first time
+   * would result in no scroll)
+   */
+  useEventualScroll();
+
+  return <div>{loading ? 'Loading...' : <p id="foo">Some text</p>}</div>;
+};
+
+test.skip('Scrolls after delay', () => {
+  render(<App />);
+});
